f50185c8d247ef815ca1513fc3a2e83057330184
Christopher Maughan <cmaughan@snarlsoftware.com>
2014-06-17 22:10:43 +0100

    stb_image.h header file

---
+++ src/stb_image.h
@@ -0,0 +1,5 @@
+// To get a header file for this, either cut and paste the header,
+// or create stb_image.h, #define STBI_HEADER_FILE_ONLY, and
+// then include stb_image.c from it.
+#define STBI_HEADER_FILE_ONLY
+#include "stb_image.c"
