37910798b5d6d7270bf1090c742d96a7e28be9a2
Christopher Maughan <cmaughan@snarlsoftware.com>
2015-02-14 12:28:22 +0000

    Make the buffer larger, temporarily - need to correctly fix buffer usage when we run out of space!

--- src/nanovg_d3d11.h
+++ src/nanovg_d3d11.h
@@ -465,7 +465,7 @@ static int D3Dnvg__renderCreate(void* uptr)
 
     // Todo: Need to find a good value for this, and
     // Use the dynamic buffer fill technnique to handle overflow
-    D3D->VertexBuffer.MaxBufferEntries = 20000;
+    D3D->VertexBuffer.MaxBufferEntries = 1000000;
     D3D->VertexBuffer.CurrentBufferEntry = 0;
 
     memset(&bufferDesc, 0, sizeof(bufferDesc));
