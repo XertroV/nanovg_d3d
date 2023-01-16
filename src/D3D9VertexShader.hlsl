d1f1dcaa78a0436bc36892a8aa1038586245a10b
Max Kaye <m@xk.io>
2023-01-16 19:40:01 +1100

    view matrix working sorta

--- src/D3D9VertexShader.hlsl
+++ src/D3D9VertexShader.hlsl
@@ -10,27 +10,27 @@ struct VS_OUTPUT
 
 VS_OUTPUT D3D9VertexShader_Main(float3 pt : POSITION, float2 tex : TEXCOORD0)
 {
-    float d = .9;
+    float d = -1.9;
     float4x4 viewMatrix = float4x4(
         float4(1.0, 0.0, .0, 0.0),
         float4(0.0, 1.0, .0, 0.0),
-        float4(0.0, 0.0, 1.0, -d),
-        float4(0, 0, 1.0/d, 0)
+        float4(0.0, 0.0, 1.0, 1.0/d),
+        float4(-1.0, -1.0, -d, 0.0)
     );
-    // float s = sin(.5);
-    // float c = cos(.5);
-    // float4x4 rot = float4x4(
-    //     float4(1.0, 0.0, 0.0, 0.0),
-    //     float4(0.0, 1.0, 0.0, 0.0),
-    //     float4(0.0, 0.0, 1.0, 0.0),
-    //     float4(0.0, 0.0, 0.0, 1.0)
-    // );
+    float s = sin(.5);
+    float c = cos(.5);
+    float4x4 rot = float4x4(
+        float4(c, 0.0, -s, 0.0),
+        float4(0.0, 1.0, 0.0, 0.0),
+        float4(s, 0.0, c, 0.0),
+        float4(0.0, 0.0, 0.0, 1.0)
+    );
 
     VS_OUTPUT Output;
     Output.ftcoord = tex;
     Output.fpos = pt;
     Output.position = float4(2.0 * pt.x / viewSize.x - 1.0, 1.0 - 2.0 * pt.y / viewSize.y, 1, 1);
-    Output.position = mul(viewMatrix, Output.position);
+    Output.position = mul(viewMatrix, mul(rot, Output.position));
 	Output.position.xy += float2(-1.0 / viewSize.x, 1.0 / viewSize.y) * Output.position.ww;
     return Output;
 }
